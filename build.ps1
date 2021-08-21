9fdc6a16fec5d3b39b3c2a34a22e72a1e5260343
Pavel Anpin <pavel@apes.ge>
2021-08-21 18:04:15 +0400

    fixed build script

--- build.ps1
+++ build.ps1
@@ -10,7 +10,6 @@ if ((Test-Path -Path ".\Build\Temp" )) {
 mkdir ".\Build\Temp"
 cp '.\CMQTT\bin\Release\CMQTT.cplz' '.\Build\Temp\'
 cp '.\CMQTT\bin\Release\CMQTT.dll' '.\Build\Temp\'
-cp '.\CMQTT\bin\Release\SSharpCrestronExtensionsLibrary.dll' '.\Build\Temp\'
 cp '.\CMQTT.nuspec' '.\Build\Temp\'
 cp '.\LICENSE.txt' '.\Build\Temp'
 .\Tools\NuGet\NuGet.exe pack .\Build\Temp\CMQTT.nuspec -OutputDirectory ".\Build\Packages"
